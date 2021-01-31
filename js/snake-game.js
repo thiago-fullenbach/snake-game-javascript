e19e999953402fbfdb7c51e57a0dd362cf8ce4de
thiago-fullenbach <thiago.fullenbach@hotmail.com>
2021-01-30 23:28:52 -0300

    c10 - Adiciona sistema de recorde

--- js/snake-game.js
+++ js/snake-game.js
@@ -5,6 +5,9 @@ window.onload = () => {
     const appleColor = 'red'
     const stageColor = 'black'
 
+    let trail = []
+    tail = 5
+
     const lp = 30
     const qp = 20
     const vel = 1
@@ -16,13 +19,17 @@ window.onload = () => {
     let ax = 15
     let ay = 10
 
-    let trail = []
-    tail = 5
-
     const lifeCounter = document.getElementById('info-lives')
     const scoreCounter = document.getElementById('info-score')
+    const TopScoreCounter = document.getElementById('info-top-score')
+
     lifeCounter.innerHTML = lives
     scoreCounter.innerHTML = tail + 1
+    
+    if(!localStorage.TopScore)
+        localStorage.TopScore = tail + 1
+    TopScoreCounter.innerHTML = localStorage.TopScore
+
     document.onkeydown = e => setDirection(e.key)
 
     function restart() { // restarts game by pressing "R" button
@@ -52,7 +59,12 @@ window.onload = () => {
     }
 
     function updateScore() {
-        scoreCounter.innerHTML = tail + 1
+        const novoComprimento = tail + 1
+        scoreCounter.innerHTML = novoComprimento
+        if(novoComprimento > localStorage.TopScore) {
+            localStorage.TopScore = novoComprimento
+            TopScoreCounter.innerHTML = novoComprimento
+        }
     }
 
     function writeMsg(text, textSize) {
@@ -72,12 +84,8 @@ window.onload = () => {
     }
 
     function newApple() { // set new apple position
-        trail.forEach(part => {
-            while(ax == part.x && ay == part.y) { // prevents apple from spawning in the snake's body
-                ax = Math.floor(Math.random()*qp)
-                ay = Math.floor(Math.random()*qp)
-            }
-        })
+        ax = Math.floor(Math.random()*qp)
+        ay = Math.floor(Math.random()*qp)
     }
 
     function setDirection(key) {
@@ -104,18 +112,18 @@ window.onload = () => {
     function checkCollision() {
         trail.forEach(part => {
             drawPiece(snakeColor, part.x * lp, part.y * lp)
-            if(part.x == px && part.y == py) { // checks collision
+            if(part.x == px && part.y == py) { // checks collision with trail parts
                 lives--
                 resetGame()
             }
         })
     }
 
-    function snakeGame() {
+    function snakeGame() { // Main function
         px += vx
         py += vy
 
-        if(px > qp - 1)
+        if(px > qp - 1) // Draw snake on the other end of the screen
             px = 0
         if(px < 0)
             px = qp - 1
